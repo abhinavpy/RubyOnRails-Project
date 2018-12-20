49664ed78989d25f1f0f47d3beb5d3b99da3e487
abhinavpy <py.abhinav2@gmail.com>
2018-12-20 10:29:36 +0530

    Added users and microposts

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources :microposts
+  resources :users
   #get 'welcome/index'
   #resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#goodbye'
+  root 'users#index'
 end
